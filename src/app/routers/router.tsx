d159005c25efa374602464d26f148477999e7f8d
Igor Samarin <samariniv@oblteh.ru>
2025-01-05 19:15:26 +0300

    Update basename

--- src/app/routers/router.tsx
+++ src/app/routers/router.tsx
@@ -3,27 +3,30 @@ import { DashboardPage } from '@/pages/dashboard-page';
 import { createBrowserRouter, Navigate } from 'react-router-dom';
 
 export const useRouter = () => {
-  return createBrowserRouter([
-    {
-      element: <Layout />,
-      children: [
-        {
-          path: 'dashboard',
-          element: <DashboardPage />,
-        },
-        {
-          path: 'dash',
-          element: <>123123</>,
-        },
-      ],
-    },
-    {
-      path: 'auth',
-      element: <AuthPage />,
-    },
-    {
-      path: '*',
-      element: <Navigate to={'dashboard'} />,
-    },
-  ], {basename: '/'});
+  return createBrowserRouter(
+    [
+      {
+        element: <Layout />,
+        children: [
+          {
+            path: 'dashboard',
+            element: <DashboardPage />,
+          },
+          {
+            path: 'dash',
+            element: <>123123</>,
+          },
+        ],
+      },
+      {
+        path: 'auth',
+        element: <AuthPage />,
+      },
+      {
+        path: '*',
+        element: <Navigate to={'dashboard'} />,
+      },
+    ],
+    { basename: '/' }
+  );
 };
